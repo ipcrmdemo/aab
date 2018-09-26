a5c155d944e3660ffa853858f70d3361c551948a
Matt Cadorette <mcadorette@gmail.com>
2018-09-26 18:55:48 -0400

    Initial commit from Atomist

---
+++ src/main/java/aab/AabApplication.java
@@ -0,0 +1,12 @@
+package aab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AabApplication.class, args);
+	}
+}
